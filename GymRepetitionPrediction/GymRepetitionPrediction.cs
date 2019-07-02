9608da76648ee917b26b813e09fab69a12390935
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-07-02 18:12:32 +0200

    Created prediction and data class

---
+++ GymRepetitionPrediction/GymRepetitionPrediction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymRepetitionPrediction
+{
+    public class GymRepetition
+    {
+        /// <summary>
+        /// Nazwa ćwiczenia
+        /// </summary>
+        [LoadColumn(0)]
+        public string Exercise;
+        /// <summary>
+        /// Ilość powtórzeń
+        /// </summary>
+        [LoadColumn(1)]
+        public int Repetitions;
+        /// <summary>
+        /// Obiążenie
+        /// </summary>
+        [LoadColumn(2)]
+        public float Weight;
+    }
+
+    public class GymRepetitionPrediction
+    {
+        /// <summary>
+        /// Przewidywane obciązenies
+        /// </summary>
+        [ColumnName("Score")]
+        public float Weight;
+    }
+}
---
+++ GymRepetitionPrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GymRepetitionPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
